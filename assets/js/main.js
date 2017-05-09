f10c0c26bdc19608b06965e3953c7de81ff9b5f1
Natalia Garrido <natgramirez@outlook.com>
2017-05-09 20:32:00 -0300

    inicializando repo y subiendo el objeto

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+var productos = [{
+        nombre: 'pijama',
+        tipo: 'ropa'
+    },
+    {
+        nombre: 'nevera',
+        tipo: 'electrodoméstico'
+    },
+    {
+        nombre: 'asus',
+        tipo: 'computador'
+    },
+    {
+        nombre: 'mcbook',
+        tipo: 'computador'
+    },
+    {
+        nombre: 'pijama',
+        tipo: 'ropa'
+    },
+    {
+        nombre: 'toshiba',
+        tipo: 'computador'
+    }
+];
+
+console.log(productos);
